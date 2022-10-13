a2eb6917346e98c698dcf24e86162b1044d3ea2b
Vladislav Sharafutdinov <sharafutdinovsvu@gmail.com>
2022-10-13 18:37:13 +0300

    second

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -2,8 +2,7 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
-
-from ..models import Group, Post
+from posts.models import Group, Post
 
 User = get_user_model()
 
